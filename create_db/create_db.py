c3a4b653f8f34d66b36655d17117939eb1133172
lta155 <1397298950@qq.com>
2024-10-10 12:05:34 +0800

    update create db

--- create_db/create_db.py
+++ create_db/create_db.py
@@ -1,4 +1,4 @@
-from langchain_text_splitters.markdown import ExperimentalMarkdownSyntaxTextSplitter
+from langchain_text_splitters.markdown import MarkdownHeaderTextSplitter
 from langchain_community.embeddings import ZhipuAIEmbeddings
 from langchain_chroma.vectorstores import Chroma
 from load_data import ipynb_load, md_load
@@ -34,18 +34,19 @@ def create_db(dir_path: str, persist_directory: str):
     file_list = get_files(dir_path=dir_path)
     docs = get_doc(file_list)
     # 将获取的docs进行分割
-    md_splitter = ExperimentalMarkdownSyntaxTextSplitter()
-    for doc in docs:
-        splitted_doc = md_splitter.split_text(doc)
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+        ("####", "Header 4"),
+        ("#####", "Header 5"),
+        ("######", "Header 6"),
+    ]
+    md_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
     documents  = []
-    # 将分割后的代码块合并到之前的内容
-    while splitted_doc:
-        current_ipynb = splitted_doc.pop(0)
-        if "Code" in current_ipynb.metadata:
-            documents[-1].page_content += "\n"
-            documents[-1].page_content += current_ipynb.page_content
-        else:
-            documents.append(current_ipynb)
+    for doc in docs:
+        documents.extend(md_splitter.split_text(doc))
+    # print(len(documents)) # 154
     # 实例化 embedding model
     embedding = ZhipuAIEmbeddings(model="embedding-3")
     # 创建向量数据库
@@ -55,9 +56,9 @@ def create_db(dir_path: str, persist_directory: str):
         persist_directory=persist_directory
     )
     # 定量向数据库中添加内容
-    for i in tqdm(range(10, len(documents), 10)):
-        vector_store.add_documents(documents=documents[i-10: i])
-    vector_store.add_documents(documents=documents[300:])
+    for i in tqdm(range(5, len(documents), 5)):
+        vector_store.add_documents(documents=documents[i-5: i])
+    vector_store.add_documents(documents=documents[150:])
     return vector_store
 if __name__ == "__main__":
     create_db(dir_path="knowledge_db/notebook", persist_directory="knowledge_db/vector_db")
